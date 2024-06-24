ea87515efab84fa6e8da7a6047a9f12c9c795ed9
Arjun I <arjun.iyyappan.2005@gmail.com>
2024-06-24 20:49:05 +0530

    Initial Commit

---
+++ test.js
@@ -0,0 +1,19 @@
+
+function dis(val) { 
+    document.getElementById("result").value += val ;
+} 
+
+function clr(){
+    document.getElementById("result").value = " ";
+}
+
+function solve(){
+    let x = document.getElementById("result").value;
+    let y = math.evaluate(x);
+    if(y == Infinity || y == NaN){
+        alert("Math Error");
+    }else{
+        document.getElementById("result").value = y;
+    }
+    
+}
